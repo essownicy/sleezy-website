e779e4a76bbf565237b3de11d5572b3a7a4ea378
adasiek193 <adas.jamro@gmail.com>
2022-12-04 22:00:01 +0100

    Merge branch 'main' of https://github.com/essownicy/sleezy-website

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-// website coded by adasiek#9999 with 💞 and zero frontend skills
-
-
 (async () => {
 	let coscoaleksbardzolubi = await (
 		await fetch(`https://ifconfig.me/ip`)
 	).text();
 
 	console.log(`do not look here pls ` + coscoaleksbardzolubi);
+
 })();
+
+// website coded by adasiek#9999 with 💞 and zero frontend skills
